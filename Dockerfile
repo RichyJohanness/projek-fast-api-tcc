75737d1bdd2114b10c6d367a9cf5375b7127c349
RichyJohanness <riickyjohaness@gmail.com>
2024-06-05 05:27:20 +0700

    d

--- Dockerfile
+++ Dockerfile
@@ -12,7 +12,7 @@ RUN pip install --no-cache-dir -r requirements.txt
 COPY . .
 
 # Ekspos port
-EXPOSE 3100
+EXPOSE 8080
 
 # Jalankan aplikasi
 CMD ["python", "dior.py"]
--- dior.py
+++ dior.py
@@ -2,10 +2,6 @@ from fastapi import FastAPI
 from typing import Optional
 from pydantic import BaseModel
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=3100)
-
-
 app = FastAPI()
 
 dior = {
